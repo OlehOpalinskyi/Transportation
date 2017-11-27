2ce9d119da06a1bbb719d7b13e69209e63232ac6
Oleh Opalinskyi <oleh.opalinskyi@gamil.com>
2017-11-27 17:31:45 +0200

    add Bus and BusRoute controllers

---
+++ Transportation/Controllers/BusController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Transportation.Data;
+using Transportation.Data.Models;
+using Transportation.Models;
+using static AutoMapper.Mapper;
+
+namespace Transportation.Controllers
+{
+    [RoutePrefix("buses")]
+    public class BusController : ApiController
+    {
+        private readonly DataContext _db;
+
+        public BusController(DataContext db)
+        {
+            _db = db;
+        }
+
+        [Route("")]
+        public IEnumerable<BusViewModel> GetBuses()
+        {
+            var dataBuses = _db.Buses;
+
+            return Map<IEnumerable<BusViewModel>>(dataBuses);
+        }
+
+        [Route("{id}")]
+        public BusViewModel GetBus(int id)
+        {
+            var dataBus = _db.Buses.Single(b => b.Id == id);
+
+            return Map<BusViewModel>(dataBus);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public BusViewModel AddBus(BusViewModel bus)
+        {
+            var dataBus = Map<BusDataModel>(bus);
+
+            _db.Buses.Add(dataBus);
+            _db.SaveChanges();
+
+            return Map<BusViewModel>(dataBus);
+        }
+        [HttpPut]
+        [Route("{id}")]
+        public BusViewModel EditBus(int id, BusViewModel bus)
+        {
+            var originBus = _db.Buses.Single(b => b.Id == id);
+            bus.Id = id;
+
+            _db.Entry(originBus).CurrentValues.SetValues(bus);
+            _db.SaveChanges();
+
+            return Map<BusViewModel>(originBus);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public BusViewModel DeleteBus(int id)
+        {
+            var bus = _db.Buses.Single(b => b.Id == id);
+
+            _db.Buses.Remove(bus);
+            _db.SaveChanges();
+
+            return Map<BusViewModel>(bus);
+        }
+    }
+}
---
+++ Transportation/Controllers/BusRoutesController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Transportation.Data;
+using Transportation.Models;
+using static AutoMapper.Mapper;
+
+namespace Transportation.Controllers
+{
+    [RoutePrefix("bus/{id}/routes")]
+    public class BusRoutesController : ApiController
+    {
+        private readonly DataContext _db;
+
+        public BusRoutesController(DataContext db)
+        {
+            _db = db;
+        }
+
+        [Route("")]
+        public IEnumerable<RouteViewModel> GetRoutes(int id)
+        {
+            var dataRoutes = _db.Buses.Single(b => b.Id == id).Routes;
+
+            return Map<IEnumerable<RouteViewModel>>(dataRoutes);
+        }
+
+        [Route("add/{routeId}")]
+        [HttpPatch]
+        public RouteViewModel AddRoute(int id, int routeId)
+        {
+            var route = _db.Routes.Single(r => r.Id == routeId);
+            _db.Buses.Single(b => b.Id == id).Routes.Add(route);
+            _db.SaveChanges();
+
+            return Map<RouteViewModel>(route);
+        }
+        [HttpDelete]
+        [Route("remove/{routeId}")]
+        public RouteViewModel RemoveRoute(int id, int routeId)
+        {
+            var bus = _db.Buses.Single(b => b.Id == id);
+            var route = bus.Routes.Single(r => r.Id == routeId);
+            bus.Routes.Remove(route);
+            _db.SaveChanges();
+
+            return Map<RouteViewModel>(route);
+        }
+    }
+}
