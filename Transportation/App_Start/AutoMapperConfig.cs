619e07371496dd482d005b675f6680453cd64a07
Oleh Opalinskyi <oleh.opalinskyi@gamil.com>
2017-11-27 14:16:33 +0200

    add AutoMapperConfig

---
+++ Transportation/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Transportation.Data.Models;
+using Transportation.Models;
+
+namespace Transportation.App_Start
+{
+    public class AutoMapperConfig
+    {
+        public static void Intialize()
+        {
+            Mapper.Initialize((config) =>
+            {
+                config.CreateMap<BusDataModel, BusViewModel>().ReverseMap();
+                config.CreateMap<CityDataModel, CityViewModel>().ReverseMap();
+                config.CreateMap<OrderDataModel, OrderViewModel>().ReverseMap();
+                config.CreateMap<PointDataModel, PointViewModel>().ReverseMap();
+                config.CreateMap<RouteDataModel, RouteViewModel>().ReverseMap();
+                config.CreateMap<TimeTableDataModel, TimeTableViewModel>().ReverseMap();
+            });
+        }
+    }
+}
--- Transportation/Global.asax.cs
+++ Transportation/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
+using Transportation.App_Start;
 
 namespace Transportation
 {
@@ -12,6 +13,7 @@ namespace Transportation
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            AutoMapperConfig.Intialize();
         }
     }
 }
