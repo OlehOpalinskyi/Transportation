fd4e0c6af5c831f22f64ee999dc80ef58d8d6633
Oleh Opalinskyi <oleh.opalinskyi@gamil.com>
2018-01-08 22:15:13 +0200

    fixed bug on CalendarService

--- Transportation/Controllers/TimeTableController.cs
+++ Transportation/Controllers/TimeTableController.cs
@@ -1,89 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
-using Transportation.Data;
-using Transportation.Data.Models;
+using Transportation.Interfaces;
 using Transportation.Models;
-using static AutoMapper.Mapper;
 
 namespace Transportation.Controllers
 {
     [RoutePrefix("timetable")]
     public class TimeTableController : ApiController
     {
-        private readonly DataContext _db;
+        private readonly ICalendarService _service;
 
-        public TimeTableController(DataContext db)
+        public TimeTableController(ICalendarService service)
         {
-            _db = db;
+            _service = service;
         }
 
-        [Route("")]
         public IEnumerable<TimeTableViewModel> GetTable()
         {
-            var dataTable = _db.TimeTable;
-
-            return Map<IEnumerable<TimeTableViewModel>>(dataTable);
+            return _service.GetTable();
         }
 
         [Route("")]
         [HttpPost]
         public TimeTableViewModel AddRecord(TimeTableUpdateModel table)
         {
-            var dataTable = CreateRecord(table);
-            _db.TimeTable.Add(dataTable);
-            _db.SaveChanges();
-
-            return Map<TimeTableViewModel>(dataTable);
+            return _service.AddRecord(table);
         }
 
         [Route("{id}")]
         [HttpPut]
         public TimeTableViewModel UpdateRecord(int id, TimeTableUpdateModel record)
         {
-            var newRecord = CreateRecord(record);
-            newRecord.Id = id;
-            var currentRecord = _db.TimeTable.Single(t => t.Id == id);
-            _db.Entry(currentRecord).CurrentValues.SetValues(newRecord);
-            _db.SaveChanges();
-
-            return Map<TimeTableViewModel>(newRecord);
+            return _service.UpdateRecord(id, record);
         }
 
         [Route("{id}")]
         [HttpPatch]
         public TimeTableViewModel ChangeDate(int id, DayOfWeek day, TimeSpan time)
         {
-            var record = _db.TimeTable.Single(t => t.Id == id);
-            record.Day = day;
-            record.Time = time;
-            _db.SaveChanges();
-
-            return Map<TimeTableViewModel>(record);
+            return _service.ChangeDate(id, day, time);
         }
 
         [Route("{id}")]
         [HttpDelete]
         public void DeleteRecord(int id)
         {
-            var record = _db.TimeTable.Single(t => t.Id == id);
-            _db.TimeTable.Remove(record);
-            _db.SaveChanges();
-        }
-
-        private TimeTableDataModel CreateRecord(TimeTableUpdateModel record)
-        {
-            var dataRecord = Map<TimeTableDataModel>(record);
-            var bus = _db.Buses.Single(b => b.Id == record.BusId);
-            var route = _db.Routes.Single(r => r.Id == record.RouteId);
-
-            dataRecord.Route = route;
-            dataRecord.Bus = bus;
-
-            return dataRecord;
+            _service.DeleteRecord(id);
         }
     }
 }
--- Transportation/Services/CalendarService.cs
+++ Transportation/Services/CalendarService.cs
@@ -1,42 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using Transportation.Data;
+using Transportation.Data.Models;
 using Transportation.Interfaces;
 using Transportation.Models;
+using static AutoMapper.Mapper;
 
 namespace Transportation.Services
 {
     public class CalendarService : ICalendarService
     {
-        private readonly ICalendarService _service;
+        private readonly DataContext _db;
 
-        public CalendarService(ICalendarService service)
+        public CalendarService(DataContext db)
         {
-            _service = service;
+            _db = db;
         }
 
         public IEnumerable<TimeTableViewModel> GetTable()
         {
-            return _service.GetTable();
+            var dataTable = _db.TimeTable;
+
+            return Map<IEnumerable<TimeTableViewModel>>(dataTable);
         }
 
         public TimeTableViewModel AddRecord(TimeTableUpdateModel table)
         {
-            return _service.AddRecord(table);
+            var dataTable = CreateRecord(table);
+            _db.TimeTable.Add(dataTable);
+            _db.SaveChanges();
+
+            return Map<TimeTableViewModel>(dataTable);
         }
 
         public TimeTableViewModel UpdateRecord(int id, TimeTableUpdateModel record)
         {
-            return _service.UpdateRecord(id, record);
+            var newRecord = CreateRecord(record);
+            newRecord.Id = id;
+            var currentRecord = _db.TimeTable.Single(t => t.Id == id);
+            _db.Entry(currentRecord).CurrentValues.SetValues(newRecord);
+            _db.SaveChanges();
+
+            return Map<TimeTableViewModel>(newRecord);
         }
 
         public TimeTableViewModel ChangeDate(int id, DayOfWeek day, TimeSpan time)
         {
-            return _service.ChangeDate(id, day, time);
+            var record = _db.TimeTable.Single(t => t.Id == id);
+            record.Day = day;
+            record.Time = time;
+            _db.SaveChanges();
+
+            return Map<TimeTableViewModel>(record);
         }
 
         public void DeleteRecord(int id)
         {
-            _service.DeleteRecord(id);
+            var record = _db.TimeTable.Single(t => t.Id == id);
+            _db.TimeTable.Remove(record);
+            _db.SaveChanges();
+        }
+
+        private TimeTableDataModel CreateRecord(TimeTableUpdateModel record)
+        {
+            var dataRecord = Map<TimeTableDataModel>(record);
+            var bus = _db.Buses.Single(b => b.Id == record.BusId);
+            var route = _db.Routes.Single(r => r.Id == record.RouteId);
+
+            dataRecord.Route = route;
+            dataRecord.Bus = bus;
+
+            return dataRecord;
         }
     }
 }
