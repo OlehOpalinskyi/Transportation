6c668edafe8425eb58c50cb916efe27606a25a39
Oleh Opalinskyi <oleh.opalinskyi@gamil.com>
2018-01-15 12:04:22 +0200

    add Startup

---
+++ Transportation/App_Start/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Transportation.App_Start.Startup))]
+
+namespace Transportation.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
